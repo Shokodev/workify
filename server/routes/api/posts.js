f9d3f7e06ef210c7f357785ba8acdf07ef9e8581
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2020-08-19 20:14:28 +0200

    -logger added -working on stable db connection

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,13 +1,21 @@
 
 const express = require('express');
 const mongodb = require('mongodb');
-
 const router = express.Router();
+const logger=require('../../logger/logger');
 
 // Get Post
 router.get('/', async (req, res) => {
-    const posts = await loadPostsCollection();
-    res.send(await posts.find({}).toArray());
+    logger.info('fetch all posts from db');
+    let sendingPosts = null;
+    await loadPostsCollection().then(posts=>{
+        sendingPosts = posts;
+        });
+    if(sendingPosts instanceof Error){
+        res.send(sendingPosts)
+    } else {
+        res.send(await sendingPosts.find({}).toArray());
+    }
 });
 
 // Add Post
@@ -30,18 +38,29 @@ router.delete('/:id', async (req, res) => {
 
 
 // Update Post
-
+router.post('/update', async (req, res) =>{
+    const posts = await loadPostsCollection();
+    await posts.updateOne({_id: new mongodb.ObjectID(req.params.id)}).then(result=>{
+        res.status(200).send();
+    });
+});
 
 
 // LCOAL MONGODB Server: mongodb://localhost:27017/workfly
 
 
-// Load PostCollection
+// Load PostCollection from DB
 async function loadPostsCollection() {
-    const client = await mongodb.MongoClient.connect('mongodb+srv://workify123:workify123@workify.iukfu.gcp.mongodb.net/workify?retryWrites=true&w=majority', {
+    let mongoDB = new Error("DB connection failed!");
+    await mongodb.MongoClient.connect('mongodb+srv://workify123:workify123@workify.iukfu.gcp.mongodb.net/workify?retryWrites=true&w=majority', {
         useNewUrlParser: true, useUnifiedTopology: true
+    }).then(dbInstance=>{
+            logger.info('connect to mongodb: ' + dbInstance.isConnected());
+            mongoDB = dbInstance.db('vue_express').collection('posts');
+    }).catch(reason => {
+        logger.error('cant connect to db: ' + reason);
     });
-    return client.db('vue_express').collection('posts');
+    return mongoDB;
 }
 
 
