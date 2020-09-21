f4d94c31de3e82691bbf2a4fc1e35e15245397b4
grimmi22 <40334510+grimmi22@users.noreply.github.com>
2020-09-21 20:31:56 +0200

    AddSettings

--- client/src/components/Settings.vue
+++ client/src/components/Settings.vue
@@ -1,7 +1,43 @@
 <template>
   <div>
     <p>Settings</p>
-    <p>{{settings}}</p>
+    <v-text-field
+        v-model="settings.calculatedGraphics"
+        label="Calculated graphic"
+        required
+    ></v-text-field>
+    <v-text-field
+        v-model="settings.plantGraphics"
+        label="Plant graphics"
+        required
+    ></v-text-field>
+    <v-text-field
+        v-model="settings.floorPlan"
+        label="Floor plan"
+        required
+    ></v-text-field>
+    <v-text-field
+        v-model="settings.navigationGraphic"
+        label="Navigation graphic"
+        required
+    ></v-text-field>
+    <v-text-field
+        v-model="settings.regulationsGraphic"
+        label="Regulations graphic"
+        required
+    ></v-text-field>
+    <v-text-field
+        v-model="settings.requiredGraphicsAtWeek"
+        label="Required graphic per week"
+        required
+    ></v-text-field>
+    <v-text-field
+        v-model="settings.totalDataPoints"
+        label="Total data points"
+        required
+    ></v-text-field>
+    <v-btn dark color="indigo"><v-icon dark>mdi-content-save</v-icon>Save</v-btn>
+
   </div>
 
 
