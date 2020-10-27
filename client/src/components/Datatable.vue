6ac3eb194c7ab14ab05e6c8f9be099592475715d
dante.reiter@gmail.com <Kakarott0$>
2020-10-27 11:24:42 +0100

    changed "edit" behavior

--- client/src/components/Datatable.vue
+++ client/src/components/Datatable.vue
@@ -11,7 +11,7 @@
     </v-progress-circular>
   </v-overlay>
     <v-data-table
-            height="800"
+            height="600"
             :headers="headers"
             :items="posts"
             item-key="_id"
@@ -19,6 +19,8 @@
             fixed-header
             dense
     >
+
+
         <template v-slot:top>
             <v-toolbar flat color="white">
                 <v-text-field
@@ -42,7 +44,118 @@
             ></AddItem>
             </v-toolbar>
         </template>
+
+
+        <template v-slot:item.item.graphic= "{ item }">
+            <v-edit-dialog
+                    :return-value.sync="item.item.graphic"
+                    large
+                    @save="save(item)"
+                    @cancel="cancel"
+            >
+                {{ item.item.graphic }}
+                <template v-slot:input>
+                    <v-text-field
+                            v-model="item.item.graphic"
+                            label="Graphic"
+                            single-line
+                    ></v-text-field>
+                </template>
+            </v-edit-dialog>
+        </template>
+        <template v-slot:item.item.selectType= "{ item }">
+            <v-edit-dialog
+                    :return-value.sync="item.item.selectType"
+                    large
+                    @save="save(item)"
+                    @cancel="cancel"
+            >
+                {{ item.item.selectType }}
+                <template v-slot:input>
+                    <v-select
+                            v-model="item.item.selectType"
+                            :items="['Floor plan', 'Navigation graphic', 'Plant graphic']"
+                            label="Type"
+                            required
+                    ></v-select>
+                </template>
+            </v-edit-dialog>
+        </template>
+        <template v-slot:item.item.regulations= "{ item }">
+            <v-edit-dialog
+                    :return-value.sync="item.item.regulations"
+                    large
+                    @save="save(item)"
+                    @cancel="cancel"
+            >
+                {{ item.item.regulations }}
+                <template v-slot:input>
+                    <v-select
+                            v-model="item.item.regulations"
+                            :items="[0, 1, 2,3,4, 5]"
+                            label="Regulations"
+                            required
+                    ></v-select>
+                </template>
+            </v-edit-dialog>
+        </template>
+        <template v-slot:item.item.selectState= "{ item }">
+            <v-edit-dialog
+                    :return-value.sync="item.item.selectState"
+                    large
+                    @save="save(item)"
+                    @cancel="cancel"
+            >
+                {{ item.item.selectState }}
+                <template v-slot:input>
+                    <v-select
+                            v-model="item.item.selectState"
+                            :items="['Not started', 'In progress', 'Finished','Issues']"
+                            label="State"
+                            required
+                    ></v-select>
+                </template>
+            </v-edit-dialog>
+        </template>
+        <template v-slot:item.item.creator= "{ item }">
+            <v-edit-dialog
+                    :return-value.sync="item.item.creator"
+                    large
+                    @save="save(item)"
+                    @cancel="cancel"
+            >
+                {{ item.item.creator }}
+                <template v-slot:input>
+                    <v-select
+                            v-model="item.item.creator"
+                            :items="['GECC 1', 'GECC 2', 'GECC 3','GECC 4']"
+                            label="Creator"
+                            required
+                    ></v-select>
+                </template>
+            </v-edit-dialog>
+        </template>
+
+
+
       <template v-slot:item.item.comments = "{ item }">
+<!--          <v-edit-dialog
+                  :return-value.sync="item.item.comments"
+                  @save="save(item)"
+                  @cancel="cancel"
+          >
+              {{ item.item.comments }}
+              <template v-slot:input>
+                  <v-textarea
+                          solo
+                          name="input-7-4"
+                          label="Comments"
+                          v-model="item.item.comments"
+                  ></v-textarea>
+              </template>
+          </v-edit-dialog>-->
+
+
         <v-tooltip bottom>
           <template v-slot:activator="{on, attrs }">
             <v-icon
@@ -52,12 +165,18 @@
                 v-bind="attrs"
                 v-on="on"
             >
+
               mdi-comment-text
             </v-icon>
           </template>
+
           <span>{{item.item.comments }}</span>
         </v-tooltip>
       </template>
+
+
+
+
       <template v-slot:item.item.siemensComments = "{ item }">
         <v-tooltip bottom>
           <template v-slot:activator="{on, attrs }">
@@ -105,6 +224,25 @@
             </v-icon>
         </template>
     </v-data-table>
+
+      <v-snackbar
+              v-model="snack"
+              :timeout="3000"
+              :color="snackColor"
+      >
+          {{ snackText }}
+
+          <template v-slot:action="{ attrs }">
+              <v-btn
+                      v-bind="attrs"
+                      text
+                      @click="snack = false"
+              >
+                  Close
+              </v-btn>
+          </template>
+      </v-snackbar>
+
   </div>
 </template>
 
@@ -176,7 +314,10 @@
                     {
                         text: 'Actions', value: 'item.actions', class: 'accent'
                     },
-                ]
+                ],
+                snack: false,
+                snackColor: '',
+                snackText: '',
             }
         },
         methods: {
@@ -193,6 +334,19 @@
             async editItem(item){
                 await PostService.editPost(item);
             },
+            async save(item) {
+                await PostService.editPost(item).then(()=> {
+                    this.snack = true;
+                    this.snackColor = 'success';
+                    this.snackText = 'Data saved';
+                })
+            },
+            cancel () {
+                this.snack = true;
+                this.snackColor = 'error';
+                this.snackText = 'Canceled'
+            },
+
             async deleteItem(item){
                 await PostService.deletePost(item._id);
                 this.loadingActive = true;
