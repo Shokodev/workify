f9492c8e27fc5dab84e2a2f36d6e01780ab32359
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2020-11-07 17:53:16 +0100

    partly fixed auth, todo is still left

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -5,13 +5,13 @@ const logger = require('../../serverlog/logger');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-//TODO check if user exists first, then check if password match
+
 router.post('/login', async (req, res, next) => {
     // Read username and password from request body
     logger.info('login from: ' + req.headers['x-forwarded-for']);
     try {
         const user = await Users.find({nickname: req.body.nickname});
-        if(!user.length === 0){
+        if(user.length !== 0){
             const match = await bcrypt.compare(req.body.password, user[0].password);
             const expiresIn = process.env.TOKEN_EXPIRES_IN
             if (match) {
